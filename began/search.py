a89bc50607a9e2caf16bcf0640ae07c7a272864e
Niklas Smedemark-Margulies <niklas.sm+github@gmail.com>
2020-03-07 19:52:25 -0500

    Add sigmoid and elu checkpoints. Add argparse to search.py

--- began/search.py
+++ began/search.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 import math
 import torch
@@ -15,14 +16,12 @@ from tqdm import trange
 import params as P
 from utils import save_img_tensorboard
 
-def load_trained_generator():
-    latest_gen = "began_gen_ckpt_29.pt"
-    #latest_gen = "ffhq_subset_10/gen_ckpt.29.pt"
+def load_trained_generator(gen_checkpoint):
     gen = SizedGenerator(P.latent_dim, P.num_filters, P.size, P.num_ups)
     try:
-        gen.load_state_dict(torch.load(os.path.join("checkpoints", latest_gen))['model_state_dict'])
+        gen.load_state_dict(torch.load(gen_checkpoint)['model_state_dict'])
     except:
-        gen.load_state_dict(torch.load(os.path.join("checkpoints", latest_gen)))
+        gen.load_state_dict(torch.load(gen_checkpoint))
 
     gen.eval()
     return gen.to('cuda:0')
@@ -47,52 +46,60 @@ def psnr(img1, img2):
 def output_to_imshow(v):
     return v.squeeze(0).detach().to('cpu').numpy().transpose(1,2,0)
 
-t = datetime.now().isoformat()
-logdir = f'tensorboard_logs/search/{t}'
-os.makedirs(logdir)
+def main(args):
+    logdir = f'tensorboard_logs/search/{args.run_name}'
+    os.makedirs(logdir)
 
-writer = SummaryWriter(logdir)
+    writer = SummaryWriter(logdir)
 
-x = load_target_image(sys.argv[1])
-save_img_tensorboard(x.squeeze(0).detach().cpu(), writer, f'original')
+    x = load_target_image(args.image)
+    save_img_tensorboard(x.squeeze(0).detach().cpu(), writer, f'original')
 
-g = load_trained_generator()
+    g = load_trained_generator(args.gen_checkpoint)
 
-n_steps = 5000
-save_every_n = 50
-n_restarts = 3
+    save_every_n = 50
 
-for i in trange(n_restarts):
-    seed = i
-    torch.manual_seed(seed)
-    np.random.seed(seed)
+    for i in trange(args.n_restarts):
+        seed = i
+        torch.manual_seed(seed)
+        np.random.seed(seed)
 
-    # Good options for starting z:
-    # - start at origin (all 0)
-    # - start from a normal centered at 0, try various values of std (starting smallest, maybe try 3 values)
-    mean = 0
-    std = 1
-    z = torch.nn.Parameter(torch.randn(64, device='cuda:0') * std + mean)
+        # Good options for starting z:
+        # - start at origin (all 0)
+        # - start from a normal centered at 0, try various values of std (starting smallest, maybe try 3 values)
+        mean = 0
+        std = 1
+        z = torch.nn.Parameter(torch.randn(64, device='cuda:0') * std + mean)
 
-    z_initial = z.data.clone()
-    optimizer = torch.optim.AdamW([z], lr=0.5e-2)
-    gamma = 2 ** (math.log(0.25, 2) / n_steps) # Want to hit 0.25*initial_lr after n_steps steps (arbitrary design)
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma)
+        z_initial = z.data.clone()
+        optimizer = torch.optim.AdamW([z], lr=0.5e-2)
+        gamma = 2 ** (math.log(0.25, 2) / args.n_steps) # Want to hit 0.25*initial_lr after n_steps steps (arbitrary design)
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma)
 
-    with torch.no_grad():
-        save_img_tensorboard(g(z_initial).squeeze(0).detach().cpu(), writer, f'restart_{i}/beginning')
+        with torch.no_grad():
+            save_img_tensorboard(g(z_initial).squeeze(0).detach().cpu(), writer, f'restart_{i}/beginning')
 
-    for j in trange(n_steps, leave=False):
-        optimizer.zero_grad()
-        #x_hat = (g(z).squeeze(0) + 1) / 2
-        x_hat = g(z).squeeze(0)
-        mse = F.mse_loss(x_hat, x)
-        mse.backward()
-        optimizer.step()
-        scheduler.step()
+        for j in trange(args.n_steps, leave=False):
+            optimizer.zero_grad()
+            #x_hat = (g(z).squeeze(0) + 1) / 2
+            x_hat = g(z).squeeze(0)
+            mse = F.mse_loss(x_hat, x)
+            mse.backward()
+            optimizer.step()
+            scheduler.step()
 
-        writer.add_scalar(f'MSE/{i}', mse, j)
-        writer.add_scalar(f'PSNR/{i}', psnr(x, x_hat), j)
+            writer.add_scalar(f'MSE/{i}', mse, j)
+            writer.add_scalar(f'PSNR/{i}', psnr(x, x_hat), j)
 
-        if j % save_every_n == 0 or j == n_steps - 1:
-            save_img_tensorboard(x_hat.squeeze(0).detach().cpu(), writer, f'restart_{i}/reconstruction', j)
+            if j % save_every_n == 0 or j == n_steps - 1:
+                save_img_tensorboard(x_hat.squeeze(0).detach().cpu(), writer, f'restart_{i}/reconstruction', j)
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('--generator_checkpoint', required=True, help="Ex.: --generator_checkpoint ./checkpoints/celeba_50k/gen_ckpt.20.pt")
+    p.add_argument('--image', required=True)
+    p.add_argument('--run_name', default=datetime.now().isoformat())
+    p.add_argument('--n_restarts', type=int, default=3)
+    p.add_argument('--n_steps', type=int, default=3000)
+    args = p.parse_args()
+    main(args)

81fd1136325c8a9f23cabb3c3279c6611d7221b6
John Park <john@roselab5.ccs.neu.edu>
2020-04-04 18:37:57 -0400

    - Finish make_compression_series - Had to transform original image and save to file

--- compress.py
+++ compress.py
@@ -31,13 +31,19 @@ import params as P
 
 LATENT_VECTOR_FILENAME = 'z.pt.gz'
 INFO_JSON_FILENAME = 'info.json'
-DEFAULT_GEN_CLASS = SizedGenerator # TODO - switch to using SimpleGenerator
-DEFAULT_GEN_CKPT = './checkpoints/celeba_cropped/gen_ckpt.49.pt'
+# DEFAULT_GEN_CLASS = SizedGenerator # TODO - switch to using SimpleGenerator
+DEFAULT_GEN_CLASS = SimpleGenerator
+DEFAULT_GEN_CKPT = './checkpoints/celeba_ELU_latent_dim_64/gen_ckpt.49.pt'
 DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-GEN_LATENT_DIM = 64 # TODO - in order to load, need to match whatever latent dim the gen has. Should figure this out from gen_ckpt filename or something, maybe store in info_json
+GEN_LATENT_DIM = 64  # TODO - in order to load, need to match whatever latent dim the gen has. Should figure this out from gen_ckpt filename or something, maybe store in info_json
 # Compression
 
-def compress(img, compression_ratio, output_filename=None, n_steps=5000, gen_ckpt=DEFAULT_GEN_CKPT):
+
+def compress(img,
+             compression_ratio,
+             output_filename=None,
+             n_steps=5000,
+             gen_ckpt=DEFAULT_GEN_CKPT):
     """
     Args:
         img - filename of image to be compressed. Must be 128x128x3 pixels
@@ -50,7 +56,9 @@ def compress(img, compression_ratio, output_filename=None, n_steps=5000, gen_ckp
     - return final z vector and the final reconstructed img
     """
     x = load_target_image(img).to(DEVICE)
-    g = load_trained_generator(DEFAULT_GEN_CLASS, gen_ckpt, latent_dim=GEN_LATENT_DIM, num_filters=P.num_filters, image_size=P.size, num_ups=P.num_ups).to(DEVICE)
+    g = load_trained_generator(DEFAULT_GEN_CLASS,
+                               gen_ckpt,
+                               latent_dim=GEN_LATENT_DIM).to(DEVICE)
     g.eval()
 
     latent_dim = get_latent_dim(compression_ratio)
@@ -82,7 +90,7 @@ def compress(img, compression_ratio, output_filename=None, n_steps=5000, gen_ckp
 
 def get_latent_dim(compression_ratio):
     # Round down to nearest integer for latent_dim
-    return (128*128*3) // compression_ratio
+    return (128 * 128 * 3) // compression_ratio
 
 
 def get_linear_layer(latent_dim, output_dim=8192):
@@ -92,9 +100,11 @@ def get_linear_layer(latent_dim, output_dim=8192):
 
 
 def write_ganz(output_filename, z, psnr, gen_ckpt):
-    info_json = { 'gen_ckpt_name': gen_ckpt, 
-                  'sha1sum': sha1sum(gen_ckpt),
-                  'PSNR': psnr}
+    info_json = {
+        'gen_ckpt_name': gen_ckpt,
+        'sha1sum': sha1sum(gen_ckpt),
+        'PSNR': psnr
+    }
     _write_ganz(output_filename, z, info_json)
     return
 
@@ -103,47 +113,56 @@ def _write_ganz(output_filename, z, info_json):
     os.makedirs(output_filename)
     # Save latent vector
 
-    with gzip.GzipFile(os.path.join(output_filename, LATENT_VECTOR_FILENAME), 'wb') as z_handle:
+    with gzip.GzipFile(os.path.join(output_filename, LATENT_VECTOR_FILENAME),
+                       'wb') as z_handle:
         print(f'DURING WRITE: z.shape: {z.shape}')
         torch.save(z.detach(), z_handle)
 
     # Save info JSON
-    with open(os.path.join(output_filename, INFO_JSON_FILENAME), 'w') as json_handle:
+    with open(os.path.join(output_filename, INFO_JSON_FILENAME),
+              'w') as json_handle:
         json.dump(info_json, json_handle)
     return
 
 
 # Uncompression
-def uncompress(input_filename, output_filename=None, gen_ckpt=DEFAULT_GEN_CKPT):
-    z, info_json = read_ganz(input_filename)
-    if sha1sum(gen_ckpt) != info_json['sha1sum']:
-        raise ValueError('Generator Checkpoints do not match - cannot uncompress!')
+def uncompress(inp, output_filename=None, gen_ckpt=DEFAULT_GEN_CKPT):
+    if isinstance(inp, str):
+        z, info_json = read_ganz(inp)
+        if sha1sum(gen_ckpt) != info_json['sha1sum']:
+            raise ValueError(
+                'Generator Checkpoints do not match - cannot uncompress!')
+    else:
+        z = inp
 
     latent_dim = z.shape[0]
     print(f'during uncompress, latent_dim {latent_dim}')
     linear_layer = get_linear_layer(latent_dim)
 
-    g = load_trained_generator(DEFAULT_GEN_CLASS, gen_ckpt, latent_dim=GEN_LATENT_DIM, num_filters=P.num_filters, image_size=P.size, num_ups=P.num_ups).to(DEVICE)
+    g = load_trained_generator(DEFAULT_GEN_CLASS,
+                               gen_ckpt,
+                               latent_dim=GEN_LATENT_DIM).to(DEVICE)
     g.eval()
 
     model_input = linear_layer(z)
     x_hat = g(model_input, skip_linear_layer=True)
     if output_filename is not None:
         save_image(x_hat, output_filename)
-    
+
     return x_hat
 
 
 def read_ganz(input_filename):
     # Read latent vector
-    with gzip.GzipFile(os.path.join(input_filename, LATENT_VECTOR_FILENAME), 'rb') as f:
+    with gzip.GzipFile(os.path.join(input_filename, LATENT_VECTOR_FILENAME),
+                       'rb') as f:
         z = torch.load(f)
         print(f'DURING WRITE: z.shape: {z.shape}')
 
     # Read info JSON
     with open(os.path.join(input_filename, INFO_JSON_FILENAME), 'r') as f:
         info_json = json.load(f)
-    
+
     return z, info_json
 
 
@@ -162,6 +181,7 @@ def sha1sum(gen_ckpt):
 
 
 class TestGANZ(unittest.TestCase):
+
     def test_all(self):
         img = './images/bananas.jpg'
 
--- figures.py
+++ figures.py
@@ -1,5 +1,10 @@
-from os.path import join
-from utils import save_gif
+from os.path import join, splitext
+from utils import jpeg_compress, load_target_image, psnr, save_gif
+import torch
+from compress import compress
+import matplotlib.pyplot as plt
+from PIL import Image, ImageMath
+import numpy as np
 """
 1) make GIFs of training
 2) for each generator, make side-by-side columns 
@@ -14,7 +19,7 @@ def make_gifs():
     folder = 'checkpoints'
     dataset = 'celeba'
 
-    for X in [64, 128, 256, 512]:
+    for X in [64, 128, 512]:
         gen_folder = join(folder, f'{dataset}_ELU_latent_dim_{X}')
 
         checkpoints = [join(gen_folder, f'gen_ckpt.{i}.pt') for i in range(50)]
@@ -22,9 +27,45 @@ def make_gifs():
         save_gif(X, checkpoints, f'latent_dim_{X}.gif')
 
 
-def make_compression_series():
-    pass
+def make_compression_series(img_fp, ratios=[2, 4, 6, 8]):
+    orig_img = load_target_image(img_fp).numpy().transpose((1, 2, 0))
+
+    # Original image after transform (now size 128 x 128 x 3)
+    transformed_img = Image.fromarray((orig_img * 255).astype(np.uint8))
+    p, ext = splitext(img_fp)
+    transformed_img_fp = f'{p}_transformed{ext}'
+    transformed_img.save(transformed_img_fp)
+
+    fig, axs = plt.subplots(len(ratios) + 1, 2, sharex=True, sharey=True)
+    axs[0, 0].imshow(orig_img)
+    axs[0, 0].set_title('Original')
+    axs[0, 0].set_xticks([])
+    axs[0, 0].set_yticks([])
+
+    fig.delaxes(ax=axs[0, 1])
+
+    for c, i in zip(ratios, range(1, len(ratios) + 1)):
+        # GAN compression
+        x_hat, _, p = compress(transformed_img_fp, c)
+        gan_img = x_hat.detach().cpu().numpy().transpose((1, 2, 0))
+        axs[i, 0].imshow(gan_img)
+        axs[i, 0].set_title(f'GAN: PSNR={p:.2f}dB')
+        axs[i, 0].set_xticks([])
+        axs[i, 0].set_yticks([])
+
+        # JPEG2000 Compression
+        jpeg_img = np.asarray(jpeg_compress(transformed_img_fp, [c],
+                                            'rates')) / 255.
+        axs[i, 1].imshow(jpeg_img)
+        p = psnr(torch.from_numpy(orig_img), torch.from_numpy(jpeg_img))
+        axs[i, 1].set_title(f'JPEG2000: PSNR={p:.2f}dB')
+        axs[i, 1].set_xticks([])
+        axs[i, 1].set_yticks([])
+
+        axs[i, 0].set_ylabel(f'Ratio={c}')
+
+    fig.savefig("./images/out.png")
 
 
 if __name__ == "__main__":
-    make_gifs()
+    make_compression_series("./images/bananas.jpg")

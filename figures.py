2733ec1043027ff64fa0ed7b9319e699cfca20b6
John Park <john@roselab5.ccs.neu.edu>
2020-04-04 21:36:23 -0400

    - make_compression_series: fixed a bit, need to do more - Add gen checkpoint for latent_dim=128

--- figures.py
+++ figures.py
@@ -27,7 +27,7 @@ def make_gifs():
         save_gif(X, checkpoints, f'latent_dim_{X}.gif')
 
 
-def make_compression_series(img_fp, ratios=[2, 4, 6, 8]):
+def make_compression_series(img_fp, ratios=[5, 10, 15, 20]):
     orig_img = load_target_image(img_fp).numpy().transpose((1, 2, 0))
 
     # Original image after transform (now size 128 x 128 x 3)
@@ -36,33 +36,42 @@ def make_compression_series(img_fp, ratios=[2, 4, 6, 8]):
     transformed_img_fp = f'{p}_transformed{ext}'
     transformed_img.save(transformed_img_fp)
 
-    fig, axs = plt.subplots(len(ratios) + 1, 2, sharex=True, sharey=True)
-    axs[0, 0].imshow(orig_img)
-    axs[0, 0].set_title('Original')
-    axs[0, 0].set_xticks([])
-    axs[0, 0].set_yticks([])
+    fig, axes = plt.subplots(len(ratios),
+                             2,
+                             figsize=(12, 6),
+                             sharex=True,
+                             sharey=True,
+                             gridspec_kw={
+                                 'hspace': 0,
+                                 'wspace': 0
+                             })
 
-    fig.delaxes(ax=axs[0, 1])
+    # axes[0, 0].imshow(orig_img)
+    # axes[0, 0].set_title('Original')
+    # axes[0, 0].set_xticks([])
+    # axes[0, 0].set_yticks([])
 
-    for c, i in zip(ratios, range(1, len(ratios) + 1)):
+    ax[0].annotate()
+
+    for c, i in zip(ratios, range(len(ratios))):
         # GAN compression
-        x_hat, _, p = compress(transformed_img_fp, c)
+        x_hat, _, p = compress(transformed_img_fp, c, n_steps=1000)
         gan_img = x_hat.detach().cpu().numpy().transpose((1, 2, 0))
-        axs[i, 0].imshow(gan_img)
-        axs[i, 0].set_title(f'GAN: PSNR={p:.2f}dB')
-        axs[i, 0].set_xticks([])
-        axs[i, 0].set_yticks([])
+        axes[i, 0].imshow(gan_img)
+        axes[i, 0].set_title(f'PSNR={p:.2f}dB')
+        axes[i, 0].set_xticks([])
+        axes[i, 0].set_yticks([])
 
         # JPEG2000 Compression
         jpeg_img = np.asarray(jpeg_compress(transformed_img_fp, [c],
-                                            'rates')) / 255.
-        axs[i, 1].imshow(jpeg_img)
+                                            'dB')) / 255.
+        axes[i, 1].imshow(jpeg_img)
         p = psnr(torch.from_numpy(orig_img), torch.from_numpy(jpeg_img))
-        axs[i, 1].set_title(f'JPEG2000: PSNR={p:.2f}dB')
-        axs[i, 1].set_xticks([])
-        axs[i, 1].set_yticks([])
+        axes[i, 1].set_title(f'PSNR={p:.2f}dB')
+        axes[i, 1].set_xticks([])
+        axes[i, 1].set_yticks([])
 
-        axs[i, 0].set_ylabel(f'Ratio={c}')
+        axes[i, 0].set_ylabel(f'Ratio={c}')
 
     fig.savefig("./images/out.png")
 

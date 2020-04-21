03e2bb2150571236efcaea7d21e5135b9f3c1642
Niklas Smedemark-Margulies <niklas.sm+github@gmail.com>
2020-04-21 17:49:33 -0400

    - Make SimpleGenerator train faster

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+import math
 
 class SimpleDiscriminator(nn.Module):
     def __init__(self, latent_dim, num_filters, image_size, num_dns, act='elu'):
@@ -9,7 +10,8 @@ class SimpleDiscriminator(nn.Module):
         else:
             raise NotImplementedError()
 
-        reduced_img_size = int(image_size / (2**num_dns))
+        self.image_size = image_size
+        reduced_img_size = int(self.image_size / (2**num_dns))
         self.shape_after_conv = reduced_img_size**2 * \
             ((num_dns+1) * num_filters)  # N_pixels x N_channels
 
@@ -22,7 +24,7 @@ class SimpleDiscriminator(nn.Module):
             *convs,
             Flatten())
         self.linear = nn.Linear(self.shape_after_conv, latent_dim, bias=False)
-        self.decoder = SimpleGenerator(latent_dim, act)
+        self.decoder = SimpleGenerator(latent_dim, self.image_size, act)
 
     def forward(self, x):
         x = self.encoder(x)
@@ -57,12 +59,16 @@ class DownBlock(nn.Module):
 
 
 class SimpleGenerator(nn.Module):
-    def __init__(self, latent_dim, act='elu'):
+    def __init__(self, latent_dim, image_size, act='elu'):
         """
         Works for images of size 128x128
         """
         super().__init__()
         self.latent_dim = latent_dim
+        self.image_size = image_size
+        if math.floor(math.log2(image_size)) != math.ceil(math.log2(image_size)):
+            raise ValueError(f"Image size must be power of 2: {image_size}")
+
         if act == 'elu':
             self.act = nn.ELU()
         else:
@@ -73,62 +79,61 @@ class SimpleGenerator(nn.Module):
 
         self.linear = nn.Linear(
             self.latent_dim, self.initial_size**2 * self.ch, bias=False)
-        self.conv_net = nn.Sequential(
-            UpBlock(up_scale=2, in_ch1=self.ch, in_ch2=self.ch, out_ch=self.ch, kern=(3, 3), stride=(
-                1, 1), pad=(1, 1), act=act, passthrough=True),  # NOTE - want this one to NOT cat(conv(x), skip(x))
-            UpBlock(up_scale=4, in_ch1=2*self.ch, in_ch2=self.ch,
-                    out_ch=self.ch, kern=(3, 3), stride=(1, 1), pad=(1, 1), act=act),
-            UpBlock(up_scale=8, in_ch1=2*self.ch, in_ch2=self.ch,
-                    out_ch=self.ch, kern=(3, 3), stride=(1, 1), pad=(1, 1), act=act),
-            UpBlock(with_skip=False,
-                    in_ch1=2*self.ch, in_ch2=self.ch, out_ch=self.ch, kern=(3, 3), stride=(1, 1), pad=(1, 1), act=act),
+
+        # first_conv is applied simply to the input
+        self.first_conv = UpBlock(self.ch, self.ch, kernel_size=(3,3), stride=(1,1), padding=(1,1), act=self.act)
+        self.first_skip = nn.Upsample(scale_factor=2, mode='nearest')
+
+        # other_convs are applied to cat(prev_layer, skip_input), so they need to accept 2*self.ch
+        n_layers = int(math.log2(self.image_size/self.initial_size))
+        self.other_convs = nn.ModuleList()
+        self.other_skips = nn.ModuleList()
+        for i in range(1, n_layers - 1):
+            self.other_convs.append(UpBlock(2*self.ch, self.ch, kernel_size=(3,3), stride=(1,1), padding=(1,1), act=self.act))
+            self.other_skips.append(nn.Upsample(scale_factor=2**(i+1), mode='nearest'))
+
+        # final_conv also applied to cat(..., skip_input), but its output will NOT be catted with anything
+        self.final_conv = nn.Sequential(
+            UpBlock(2*self.ch, self.ch, kernel_size=(3,3), stride=(1,1), padding=(1,1), act=self.act),
             nn.Conv2d(self.ch, 3, kernel_size=(3, 3), padding=1, bias=False),
             self.act)
 
     def forward(self, x, skip_linear_layer=False):
         if not skip_linear_layer:
             x = self.linear(x)
-        x = x.view(-1, self.ch, self.initial_size, self.initial_size)
-        x = self.conv_net((x, x))
-        return x
+        x_0 = x.view(-1, self.ch, self.initial_size, self.initial_size)
+
+        conv_output = self.first_conv(x_0)
+        skip_output = self.first_skip(x_0)
+
+        for conv, skip in zip(self.other_convs, self.other_skips):
+            conv_output = conv(torch.cat((conv_output, skip_output), dim=1))
+            skip_output = skip(x_0)
+
+        return self.final_conv(torch.cat((conv_output, skip_output), dim=1))
 
 
 class UpBlock(nn.Module):
-    def __init__(self, with_skip=True, up_scale=None, in_ch1=None, in_ch2=None, passthrough=False, **kwargs):
+    def __init__(self, in_ch1=None, in_ch2=None, **kwargs):
         super().__init__()
-        self.with_skip = with_skip
         self.conv = nn.Sequential(
             nn.Upsample(scale_factor=2, mode='nearest'),
-            # <- should expect 256 in, produces 128
-            ConvBlock(in_ch=in_ch1, **kwargs),
-            ConvBlock(in_ch=in_ch2, **kwargs))  # <- only gets 128 in
+            ConvBlock(in_ch=in_ch1, out_ch=in_ch2, **kwargs), # <- should expect 256 in, produces 128
+            ConvBlock(in_ch=in_ch2, out_ch=in_ch2, **kwargs))  # <- only gets 128 in
 
-        if self.with_skip:
-            self.skip = nn.Upsample(scale_factor=up_scale)
-
-    def forward(self, conv_input_skip_input):
-        conv_input, skip_input = conv_input_skip_input
-        conv_output = self.conv(conv_input)
-        if self.with_skip:
-            skip_output = self.skip(skip_input)
-            return torch.cat((conv_output, skip_output), dim=1), skip_input
-        else:
-            return conv_output
+    def forward(self, x):
+        return self.conv(x)
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, kern, stride, pad, act='elu'):
+    def __init__(self, in_ch, out_ch, act='elu', **kwargs):
         super().__init__()
-        if act == 'elu':
-            self.act = nn.ELU
-        else:
-            raise NotImplementedError()
-
+        self.act = act
         self.net = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, kern, stride, pad, bias=False),
+            nn.Conv2d(in_ch, out_ch, bias=False, **kwargs),
             # TODO - add batchnorm
             #            nn.BatchNorm2d(out_ch),
-            self.act())
+            self.act)
 
     def forward(self, x):
         return self.net(x)
